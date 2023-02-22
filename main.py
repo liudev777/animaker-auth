f1c0bb8a83dcacfd40ff4dfe49e1cc75b8d07294
Devin Liu <devin@Devins-MacBook-Pro.local>
2023-02-21 22:45:25 -0600

    token

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import urllib.parse
 dotenv.load_dotenv()
 CLIENT_ID = os.environ["CLIENT_ID"]
 CLIENT_SECRET = os.environ["CLIENT_SECRET"]
-REDIRECT_URI = os.environ["REDIRECT_URI"]
 
 
 
@@ -30,13 +29,13 @@ def index():
     elif not (discordId):
         return "Please sign in through discord. Contact kewb#7881 for inquiry"
     else:
-        authUrl = f'https://anilist.co/api/v2/oauth/authorize?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&state={discordId}&response_type=code'
+        REDIRECT_URI = request.url
 
         oauth = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI)
-        authorization_url, state = oauth.authorization_url(authUrl)
+        # authorization_url, state = oauth.authorization_url(authUrl)
         tokenUrl = 'https://anilist.co/api/v2/oauth/token'
         try:
-            token = oauth.fetch_token(tokenUrl, code=code, client_secret=CLIENT_SECRET)
+            token = oauth.fetch_token(tokenUrl, code=code, client_secret=CLIENT_SECRET)['access_token']
         except Exception as e:
             print(e)
             return "Something went wrong with the token, please contact kewb#7881 on discord"
