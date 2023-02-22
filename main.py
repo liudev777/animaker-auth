3b9f9c1652d54c2ef9b16c11afb2a4cfe871d4d5
Devin Liu <devin@Devins-MacBook-Pro.local>
2023-02-21 22:47:33 -0600

    fixed

--- main.py
+++ main.py
@@ -23,35 +23,36 @@ def index():
 
     code = request.args.get('code') #anilist auth will auto append this after user auths and open up this
     discordId = request.args.get('state')
-    discordId = int(decrypt(discordId).decode())
     if not code:
         return "Code not provided"   
     elif not (discordId):
         return "Please sign in through discord. Contact kewb#7881 for inquiry"
-    else:
-        REDIRECT_URI = request.url
+    discordId = int(decrypt(discordId).decode())    
+    print("CODE IS: ", code)
+    print("Discord ID IS: ", discordId)
+    REDIRECT_URI = request.url
 
-        oauth = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI)
-        # authorization_url, state = oauth.authorization_url(authUrl)
-        tokenUrl = 'https://anilist.co/api/v2/oauth/token'
-        try:
-            token = oauth.fetch_token(tokenUrl, code=code, client_secret=CLIENT_SECRET)['access_token']
-        except Exception as e:
-            print(e)
-            return "Something went wrong with the token, please contact kewb#7881 on discord"
-        
-        try:
-            encrypted_token = encrypt(str(token))
-        except Exception as e:
-            print(e)
-            return "An internal error occured with encryption"
-        
-        try:
-            insertData(discordId=discordId, anilistToken=encrypted_token)
-            return f"You can close this page and go back to Discord!"
-        except Exception as e:
-            print (e)
-            return "An internal error occured with database"
+    oauth = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI)
+    # authorization_url, state = oauth.authorization_url(authUrl)
+    tokenUrl = 'https://anilist.co/api/v2/oauth/token'
+    try:
+        token = oauth.fetch_token(tokenUrl, code=code, client_secret=CLIENT_SECRET)['access_token']
+    except Exception as e:
+        print(e)
+        return "Something went wrong with the token, please contact kewb#7881 on discord"
+    
+    try:
+        encrypted_token = encrypt(str(token))
+    except Exception as e:
+        print(e)
+        return "An internal error occured with encryption"
+    
+    try:
+        insertData(discordId=discordId, anilistToken=encrypted_token)
+        return f"You can close this page and go back to Discord!"
+    except Exception as e:
+        print (e)
+        return "An internal error occured with database"
     
 
 def startWeb():
